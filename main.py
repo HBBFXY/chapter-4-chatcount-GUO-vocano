77e8a9b610d5227b656b703a18bedd9d937cfae4
GUO-vocano <1605776459@qq.com>
2025-10-17 11:26:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
-
-# 从键盘获取一行输入
 input_str = input("请输入一行字符：")
-
-# 遍历输入的每一个字符
 for char in input_str:
     if char.isalpha():  # 判断是否为英文字符（字母）
         letter_count += 1
@@ -17,8 +13,6 @@ for char in input_str:
         space_count += 1
     else:  # 其他情况，即为其他字符
         other_count += 1
-
-# 按照要求格式输出统计结果
 print(f"英文字符: {letter_count}")
 print(f"数字: {digit_count}")
 print(f"空格: {space_count}")
